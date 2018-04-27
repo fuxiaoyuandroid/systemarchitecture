b5ddd973a23c36cb0b572bb31a6ef0e37c30ceb2
jt <service@sdjtbd.com>
2018-04-27 09:54:03 +0800

    20180427  系统架构的开始

---
+++ app/src/main/java/com/jt/systemarchitecture/MainActivity.java
@@ -0,0 +1,16 @@
+package com.jt.systemarchitecture;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+/**
+ * 20180427 系统架构
+ */
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
